9659a3aceeaca1fd65ebb3df3099befd71dc3612
KEENTO <handball0321@gmail.com>
2023-04-03 09:47:04 -0700

    Completed creating TopHeader component

--- src/components/topHeader/TopHeader.tsx
+++ src/components/topHeader/TopHeader.tsx
@@ -2,25 +2,28 @@ import { topHeaderTexts } from "../../constants/topHeader";
 import { BsFillPersonFill } from "react-icons/bs";
 import { TbClockHour3 } from "react-icons/tb";
 import styles from "./styles.module.scss";
+import VerticalDivider from "../divider/VerticalDivider";
 
 export default function TopHeader() {
   return (
     <div className={styles.topHeader}>
       <div className={styles.topHeader_container}>
         <section className={styles.topHeader_content}>
-          <p>{topHeaderTexts[0]}</p>
+          <p className={styles.topHeader_text}>{topHeaderTexts[0]}</p>
+          <VerticalDivider />
         </section>
         <section className={styles.topHeader_content}>
           <span className={styles.topHeader_icon}>
             <BsFillPersonFill />
           </span>
-          <p>{topHeaderTexts[1]}</p>
+          <p className={styles.topHeader_text}>{topHeaderTexts[1]}</p>
+          <VerticalDivider />
         </section>
         <section className={styles.topHeader_content}>
           <span className={styles.topHeader_icon}>
             <TbClockHour3 />
           </span>
-          <p>{topHeaderTexts[2]}</p>
+          <p className={styles.topHeader_text}>{topHeaderTexts[2]}</p>
         </section>
       </div>
     </div>
