a24323ddbb061146a92f9952a04657e2517048e8
KEENTO <handball0321@gmail.com>
2023-04-03 10:24:14 -0700

    Compelted creating UI of MainHeader component

--- src/components/mainHeader/MainHeader.tsx
+++ src/components/mainHeader/MainHeader.tsx
@@ -1,11 +1,17 @@
 import SearchButton from "../button/SearchButton";
 import SubScribeButton from "../button/SubScribeButton";
+import styles from "./styles.module.scss";
 
 export default function MainHeader() {
   return (
-    <div>
-      <SubScribeButton />
-      <SearchButton />
-    </div>
+    <header className={styles.mainHeader}>
+      <section className={styles.mainHeader_title}>
+        <h2>Hacker News</h2>
+      </section>
+      <section className={styles.mainHeader_buttons}>
+        <SubScribeButton />
+        <SearchButton />
+      </section>
+    </header>
   );
 }
