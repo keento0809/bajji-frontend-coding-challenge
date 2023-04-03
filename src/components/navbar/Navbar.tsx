5f48d8d30019a6ff7c2352a28e844c3b8c391d65
KEENTO <handball0321@gmail.com>
2023-04-03 10:38:45 -0700

    Completed creating default UI of Navbar component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import { navMenuContent } from "../../constants/navbar";
 import styles from "./styles.module.scss";
 
 export default function Navbar() {
+  //TODO:Add a logic to change styles of the selected nav menu
+  // React state managing the current selected nav menu (Default value is "Top")
+  const [activeNavMenu, setActiveNavMenu] = useState("Top");
+
   return (
     <nav className={styles.navbar}>
       {navMenuContent.map((content) => {
         return (
-          <span key={content} className={styles.navbar_menu}>
+          <span
+            key={content}
+            className={
+              content === activeNavMenu
+                ? styles.navbar_menuActive
+                : styles.navbar_menu
+            }
+          >
             {content}
           </span>
         );
