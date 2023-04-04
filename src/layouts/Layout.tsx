2f7a2bd2da38e3c231a9df673572dd9d10e9139a
KEENTO <handball0321@gmail.com>
2023-04-04 09:38:35 -0700

    Created loader component and added it to Layout

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -5,14 +5,18 @@ import Navbar from "../components/navbar/Navbar";
 import MainContentWrapper from "../components/wrapper/MainContentWrapper";
 import NewsContentWrapper from "../components/wrapper/NewsContentWrapper";
 import Footer from "../components/footer/Footer";
+import Loader from "../components/loader/Loader";
+import useLoadingContext from "../hooks/useLoadingContext";
 
 interface Props {
   children: ReactNode;
 }
 
 export default function Layout({ children }: Props) {
+  const { isLoading } = useLoadingContext();
   return (
     <>
+      {isLoading && <Loader />}
       <TopHeader />
       <MainContentWrapper>
         <MainHeader />
