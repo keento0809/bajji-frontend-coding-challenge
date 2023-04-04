ff137a0aefc415783dfc00c9d475bd51d9cb5300
KEENTO <handball0321@gmail.com>
2023-04-03 17:05:43 -0700

    Cleaned up component structures in Home view page

--- src/features/section/TopLatestNewsSection.tsx
+++ src/features/section/TopLatestNewsSection.tsx
@@ -1,3 +1,22 @@
+import LoadMoreNewsButton from "../../components/button/LoadMoreNewsButton";
+import Headline from "../../components/headline/Headline";
+import NewsList from "../../components/list/NewsList";
+import LatestNewsBlock from "../block/LatestNewsBlock";
+import styles from "./styles.module.scss";
+
 export default function TopLatestNewsSection() {
-  return <div>TopLatestNewsSection</div>;
+  return (
+    <div className={styles.topLatestNewsSection}>
+      <Headline />
+      <div className={styles.topLatestNewsSection_news}>
+        <div>
+          <NewsList />
+          <LoadMoreNewsButton />
+        </div>
+        <div>
+          <LatestNewsBlock />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,27 +1,11 @@
-import Headline from "../components/headline/Headline";
-import NewsList from "../components/list/NewsList";
-import LatestNewsBlock from "../features/block/LatestNewsBlock";
-import CategorizedNewsBlock from "../features/block/CategorizedNewsBlock";
 import CategoryNewsSection from "../features/section/CategoryNewsSection";
+import TopLatestNewsSection from "../features/section/TopLatestNewsSection";
 
 export default function Home() {
   return (
-    <div>
-      <Headline />
-      {/* TODO:Refactor styling for this div tag later */}
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          gap: "48px",
-        }}
-      >
-        <NewsList />
-        <LatestNewsBlock />
-      </div>
-      {/* TODO:Refactor these three CategorizedNewsBlock components later. */}
+    <>
+      <TopLatestNewsSection />
       <CategoryNewsSection />
-    </div>
+    </>
   );
 }
