3ef50b5b4384d041175e5d611ecb061c888950eb
KEENTO <handball0321@gmail.com>
2023-04-03 17:31:35 -0700

    Applied NewsData from API to Headline component

--- src/features/section/TopLatestNewsSection.tsx
+++ src/features/section/TopLatestNewsSection.tsx
@@ -3,11 +3,20 @@ import Headline from "../../components/headline/Headline";
 import NewsList from "../../components/list/NewsList";
 import LatestNewsBlock from "../block/LatestNewsBlock";
 import styles from "./styles.module.scss";
+import useFetchNews from "../../hooks/useFetchNews";
 
 export default function TopLatestNewsSection() {
+  // Define url for fetching top-stories news from API
+  const url = `https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty&limitToFirst=19&orderBy="$key"`;
+
+  // Declare useFetchNews custom hook with url above
+  const { news, fetchNews } = useFetchNews(url);
+
+  // Retrieve the news data without News for headline component
+  const headlineNews = news?.slice(1, news.length);
   return (
     <div className={styles.topLatestNewsSection}>
-      <Headline />
+      <Headline headlineNews={news[0]} />
       <div className={styles.topLatestNewsSection_news}>
         <div>
           <NewsList />
