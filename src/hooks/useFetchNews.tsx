4567e06a6fbe1248d03854d40add7567204871d1
KEENTO <handball0321@gmail.com>
2023-04-04 09:48:20 -0700

    Introduced useLoadingContext to useFetchNews custom hook

--- src/hooks/useFetchNews.tsx
+++ src/hooks/useFetchNews.tsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from "react";
 import { NewsData } from "../types/news";
+import useLoadingContext from "./useLoadingContext";
 
 export default function useFetchNews(url: string) {
   const [news, setNews] = useState<NewsData[]>([]);
+  const { handleTurnOnLoader, handleTurnOffLoader } = useLoadingContext();
   const translatedNewsDataArray: NewsData[] = [];
 
-  const translateNewsData = async (newsId: number) => {
+  // function translates newsId to detailed news data
+  async function translateNewsData(newsId: number) {
+    // Get detailed news data by using newsId
     const response = await fetch(
       `https://hacker-news.firebaseio.com/v0/item/${newsId}.json?print=pretty`
     );
@@ -15,20 +19,25 @@ export default function useFetchNews(url: string) {
       return news?.id === translatedNews?.id;
     });
     if (!isTranslatedNewsExist) translatedNewsDataArray.push(translatedNews);
-  };
+  }
 
-  const fetchNews = async (url: string) => {
+  // function fetch news (array of number) and update news data
+  async function fetchNews(url: string) {
     try {
+      // Turn on loading animation
+      handleTurnOnLoader();
       const response = await fetch(url);
       const newsData: number[] = await response.json();
       for (const newsId of newsData) {
         await translateNewsData(newsId);
       }
       setNews(translatedNewsDataArray);
+      // Turn off loading animation
+      handleTurnOffLoader();
     } catch (error) {
       console.log(error);
     }
-  };
+  }
 
   useEffect(() => {
     fetchNews(url);
