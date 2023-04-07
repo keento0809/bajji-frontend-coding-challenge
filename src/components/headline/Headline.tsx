a8a53a6051ade2a3d20946b7d62619ddd76a57d6
KEENTO <handball0321@gmail.com>
2023-04-07 09:08:00 -0700

    Modified styles for components to match perfectly to figma design

--- src/components/headline/Headline.tsx
+++ src/components/headline/Headline.tsx
@@ -19,19 +19,19 @@ export default function Headline({ headlineNews }: Props) {
       <section className={styles.headline_titleSection}>
         {isFetching === 0 && <p>{headlineNews?.type}</p>}
         <div className={styles.headline_title}>
-          <h1>
-            {headlineNews?.title}{" "}
-            {headlineNews?.url && `(${headlineNews?.url})`}
-          </h1>
+          {isFetching === 0 && (
+            <h1>
+              {headlineNews?.title}{" "}
+              {headlineNews?.url && `(${headlineNews?.url})`}
+            </h1>
+          )}
         </div>
       </section>
       <section className={styles.headline_authorAndTimeSection}>
         {isFetching === 0 && (
           <span className={styles.headline_author}>by {headlineNews?.by}</span>
         )}
-        {isFetching === 0 && (
-          <span className={styles.headline_time}>{fixedTime && fixedTime}</span>
-        )}
+        {isFetching === 0 && <span>{fixedTime && fixedTime}</span>}
       </section>
     </div>
   );
